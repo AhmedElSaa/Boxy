8d29d191bd33fb76b944f1440e8352205e635818
Ahmed EL SAADAWY <94537516+Ahmed93Rms@users.noreply.github.com>
2025-07-14 13:13:42 +0200

    Boxy generator terminé

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <footer className="p-2 border-t-2 border-gray-200">
+        <p className="text-center uppercase text-sm">Boxy - All right reserved &copy;</p>
+    </footer>
+  )
+}
